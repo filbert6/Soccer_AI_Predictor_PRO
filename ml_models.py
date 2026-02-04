d7eb5b5e617371b93629d4aa16d8dc5d31492d5a
filbert6 <filbert6.dembele.fd@gmail.com>
2026-02-04 23:14:37 +0000

    Add initial machine learning model functions

---
+++ ml_models.py
@@ -0,0 +1,20 @@
+# Ici, tu peux mettre tes modèles ML/DL
+# Exemple simplifié avec placeholders
+
+def logistic_regression(features):
+    return "2-1"
+
+def random_forest(features):
+    return "1-1"
+
+def xgboost_model(features):
+    return "2-0"
+
+def svm_model(features):
+    return "1-0"
+
+def fnn_model(features):
+    return "2-1"
+
+def rnn_model(features):
+    return "1-1"
